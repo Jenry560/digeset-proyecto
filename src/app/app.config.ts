461a3f1e46560157dfcd741a16c6355bc42dd48b
Jenry560 <115893645+Jenry560@users.noreply.github.com>
2024-11-27 16:03:47 -0400

    second comit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,18 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideToastr } from 'ngx-toastr';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HeadersInterceptor } from './core/HeadersInterceptor';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+
+    provideToastr(),
+    provideAnimations(),
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([HeadersInterceptor])),
+  ],
 };
---
+++ src/app/core/HeadersInterceptor.ts
@@ -0,0 +1,55 @@
+import Swal from 'sweetalert2';
+
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandlerFn,
+  HttpInterceptorFn,
+  HttpRequest,
+} from '@angular/common/http';
+import { Observable, catchError, finalize, throwError } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+export const HeadersInterceptor: HttpInterceptorFn = (
+  req: HttpRequest<unknown>,
+  next: HttpHandlerFn
+): Observable<HttpEvent<unknown>> => {
+  let auth = new AuthService(); //crear instancia de Auth
+
+  const token = auth.getToken(); //obtener token
+  //Si existe token, agregarlo en las cabeceras
+  if (token) {
+    req = req.clone({
+      headers: req.headers.set('Authorization', `Bearer ${token}`),
+    });
+  }
+  if (AuthService.loadingHeader) {
+    //Cargando swal en el interceptor
+    Swal.fire({
+      title: 'Espere por favor...',
+      allowEscapeKey: false,
+      allowOutsideClick: false,
+      didOpen: () => {
+        Swal.showLoading(null);
+      },
+      willClose: () => {
+        Swal.close();
+      },
+    });
+  }
+
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      // Si el token ha expirado, cerrar la sesión
+      if (error.status === 401) {
+        auth.logout(); //cerrar sesión
+      }
+      return throwError(() => error);
+    }),
+    finalize(() => {
+      if (AuthService.loadingHeader) {
+        Swal.close(); //cerrar el loading
+      }
+    })
+  );
+};
